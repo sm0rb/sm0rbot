ca7629ea951047cca58f76599c20c249109c9ad7
sm0rb <70565615+sm0rb@users.noreply.github.com>
2021-04-21 03:24:30 +0300

    main

--- config.py
+++ config.py
@@ -1,1 +1,8 @@
-TOKEN = '1702052811:AAGsv86KRbXyzzQCVu50YXHuHSmdtLiDayc' #токен бота @Sm0rbot
+'''
+1702052811:AAGsv86KRbXyzzQCVu50YXHuHSmdtLiDayc - основной
+1746146042:AAHdA4KzDh4BpfjhtmbjC_9j3WA_K6ml4pg - тестер
+'''
+TOKEN = '1746146042:AAHdA4KzDh4BpfjhtmbjC_9j3WA_K6ml4pg' #токен тест-бота @Sm0rbot
+boboltyshkaBool = False #переменная для собеседника
+helloBool = False #переменная для приветствия
+menuBool = False #переменная для меню
--- main.py
+++ main.py
@@ -1,47 +1,70 @@
 #Подключение библиотек
-import telebot 
+import telebot
+from telebot import types
+import sqlite3
+#Подключение файлов
 import config
+import keyboars as k
 from boltyshka import *
 
-from telebot import types
-
 bot = telebot.TeleBot(config.TOKEN) # создание объекта класса бот
+
+# инициализируем соединение с БД
 
 #хрень с командами
 @bot.message_handler(commands=['start'])
 def welcome(message):
+    config.boboltyshkaBool = False
+    config.helloBool = True
+    config.menuBool = False
+
+    #добавляем юзера в БД
+
     sti = open('Stikers/AnimatedSticker.tgs', 'rb')
     bot.send_sticker(message.chat.id, sti)
 
-    markup = types.InlineKeyboardMarkup(row_width=1)
-    item1 = types.InlineKeyboardButton('Я твой создатель👨🏼‍💻', callback_data='1')
-    item2 = types.InlineKeyboardButton('Я его невеста👰🏻', callback_data='2')
-    item3 = types.InlineKeyboardButton('Я знаю его и с тебя хватит на этом😏', callback_data='3')
+    bot.send_message(message.chat.id, '{0.first_name}, рад тебя видеть!😍\nЯ - <b>{1.first_name}</b>☀️, создан рыжим программистом👨🏼‍💻, чтобы его невесте👰🏻 было не одиноко, когда он не в сети.\nКем ты приходишься для моего создателя?🤔'.format(message.from_user, bot.get_me()), 
+        parse_mode='html',reply_markup=k.hello)
 
-    markup.add(item1, item2, item3)
+@bot.message_handler(commands=['menu'])
+def menu(message):
+    config.boboltyshkaBool = False
+    config.helloBool = False
+    config.menuBool = True
 
-    bot.send_message(message.chat.id, '{0.first_name},рад тебя видеть!😍\nЯ - <b>{1.first_name}</b>☀️, создан рыжим программистом👨🏼‍💻, чтобы его невесте👰🏻 было не одиноко, когда он не в сети.\nКем ты приходишься для моего создателя?🤔'.format(message.from_user, bot.get_me()), 
-        parse_mode='html',reply_markup=markup)
+    bot.send_message(message.chat.id, '🔡Добро пожаловать в меню🔢\nЧто ты хочешь чтобы я сделал?🤔',
+    reply_markup=k.menu)
 
 #хрень с сообщениями
 @bot.message_handler(func=lambda message: True)
 def echo_all(message):
-    bot.reply_to(message, pipe.predict([message.text.lower()])[0])
+    if config.boboltyshkaBool == True:
+        bot.reply_to(message, pipe.predict([message.text.lower()])[0])
+    else:
+        bot.send_message(message.chat.id, 'Прости, но я не знаю чего ты хочешь от меня😓')
 
-#ответы бота на приветствие
+#ответы бота 
 @bot.callback_query_handler(func=lambda call: True)
 def callback_inline(call):
     try:
         if call.message:
-            if call.data == '1':
-                bot.send_message(call.message.chat.id, 'О, мой Повелитель!😇\nПришел редачить меня?😏\nХочу новые функции😢')
-            elif call.data == '2':
-                bot.send_message(call.message.chat.id, 'Оу-у-у, как долго я тебя ждал❤️\nВкраце:\n-Я первая версия1️⃣\n-Я умею здороваться👋\n-Умею нести чушь😜 но это же лучше, чем ничего?)))\n*напиши что угодно и мои ответы понесуться')
-            elif call.data == '3':
-                bot.send_message(call.message.chat.id, 'Вау, ну и дерзость😯\nЯ тебе ничего не расскажу, сам гадай!😡')
+            if config.helloBool == True: #ответы на приветствие
+                if call.data == 'H1':
+                    bot.send_message(call.message.chat.id, 'О, мой Повелитель!😇\nПришел редачить меня?😏\nХочу новые функции😢')
+                elif call.data == 'H2':
+                    bot.send_message(call.message.chat.id, 'Оу-у-у, как долго я тебя ждал❤️\nВкраце:\nЯ версия номер:2️⃣\nВызови 🔡меню🔢 командой, чтобы с мной взаимодействовать😊\n📝P.S.:Нажми или введи "/"📝')
+                elif call.data == 'H3':
+                    bot.send_message(call.message.chat.id, 'Вау, ну и дерзость😯\nЯ тебе ничего не расскажу, сам гадай!😡')
 
-            #удаление inline сообщений
-            bot.edit_message_reply_markup(call.message.chat.id, call.message.message_id)
+                #удаление inline сообщений
+                bot.edit_message_reply_markup(call.message.chat.id, call.message.message_id)
+            elif config.menuBool == True: #ответы меню
+                if call.data == 'M1':
+                    bot.send_message(call.message.chat.id, '💁🏼‍♂️Включен режим собеседника💁🏼‍♂️\n⛔️Если захочешь прекратить⛔️\nто просто вызови 🔡меню🔢\nТеперь можем поболтать😇')
+                    config.boboltyshkaBool = True
+
+                    #удаление inline сообщений
+                    bot.edit_message_reply_markup(call.message.chat.id, call.message.message_id)
 
     except Exception as e:
         print(repr(e))
